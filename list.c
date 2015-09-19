248183b4dde26abfe9f667434483be7fad9ca8a0
Colin Szechy <cszechy@umich.edu>
2015-09-19 18:40:18 -0400

    Committed to the correct branch, after accidentally committing to the wrong one locally and pushing that up to GitHub

--- list.c
+++ list.c
@@ -10,6 +10,42 @@
 list_t* insert_sorted(list_t* head, list_t* new_element) {
 	assert(head != NULL);
 	assert(new_element != NULL);
+	
+	list_t* first = head;
+	list_t* second = head->next;
+	uint8_t inserted = 0;
+	
+	// Check if new_element should go up front	
+	if(head->index > new_element->index) {
+		new_element -> next = head;
+		head = new_element;
+		inserted = 1;
+	}	
+	// Check against each possible move until it's greater then new_element, or quit
+	while(second && !inserted) {
+		// if new_element fits nicely between the first and second
+		if((new_element->index > first->index) && (new_element->index < second-> index)) {
+			first -> next = new_element;
+			new_element -> next = second;
+			inserted = 1;
+		}
+		// advance second and first
+		first = first -> next;
+		second = second -> next;
+	}
+
+	// Check if new_element should go on the end
+	if(!inserted) {
+		first->next = new_element;
+		new_element->next = NULL;
+	}
+
+	// Do my own printing
+	list_t* index = head;
+	char string_for_int[35];
+	while(index) {
+		index = index->next;
+	}
 
 	return head;
 }
@@ -18,7 +54,23 @@ list_t* insert_sorted(list_t* head, list_t* new_element) {
 // the resulting list. You do not need to preserve the original list.
 list_t* reverse(list_t* head) {
 	assert(head != NULL);
+	// Have three pointers to keep track of various objects
+	list_t* first = NULL;
+	list_t* second = head;
+	list_t* third = head->next;
 
-	return head;
+	while(second->next) {
+		// set second to point to first
+		second->next = first;
+		// advance first
+		first = second;
+		// advance second
+		second = third;
+		// advance third
+		third = third->next;
+	}
+	second->next = first;
+
+	return second;
 }
 
